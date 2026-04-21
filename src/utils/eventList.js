4e3b14b44edd438729a4badeadf91b173c293b5f
Arpit Gupta <arpitgupta14786@gmail.com>
2026-04-21 06:17:26 +0100

    added event ticket booking

---
+++ src/utils/eventList.js
@@ -0,0 +1,6 @@
+export const EVENT_TICKETS = [
+  { id: 1, name: "Freshers Party", price: 50 },
+  { id: 2, name: "Tech Symposium", price: 100 },
+  { id: 3, name: "Annual Gala", price: 150 },
+  { id: 4, name: "Coding Bootcamp", price: 75 }
+];
